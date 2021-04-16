97538861ab92425912349f638757f6e0b31f2385
Mami Goshozono <Mami@Mamis-MacBook-Air.local>
2021-04-15 23:04:39 -0500

    Mac feature 1

--- src/main/java/com/example/prueba/PruebaApplication.java
+++ src/main/java/com/example/prueba/PruebaApplication.java
@@ -9,6 +9,7 @@ public class PruebaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaApplication.class, args);
 		System.out.println("se crea el cambio para un feature");
+		System.out.println("actualiza info en feature uno. desde Mac");
 	}
 
 }
